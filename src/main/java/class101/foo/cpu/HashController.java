800a215ab11fe6636c63e607925d2fab30af9498
yoogj0513 <yoogj0513@naver.com>
2022-04-29 22:57:19 +0900

    change message

--- src/main/java/class101/foo/cpu/HashController.java
+++ src/main/java/class101/foo/cpu/HashController.java
@@ -21,7 +21,7 @@ public class HashController {
 
     @RequestMapping("/hello")
     public String hello() {
-        return "hello deploy 2";
+        return "hello deploy 3";
     }
 
     private String getMD5Digest(String input) throws NoSuchAlgorithmException {
